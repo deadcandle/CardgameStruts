06b16beba5518d32fce256b2ec69f76a514baf4c
Roï Eielts <103494921+Roi-Eielts@users.noreply.github.com>
2022-06-14 08:37:53 +0200

    java --> database database --> java

--- src/main/java/prg/Welcome.java
+++ src/main/java/prg/Welcome.java
@@ -3,8 +3,9 @@ package prg;
 import com.opensymphony.xwork2.ActionSupport;
 public class Welcome extends ActionSupport {
     private String input;
-    private String output;
+    private String beginKaart;
     public String execute() {
+
         return SUCCESS;
     }
 
