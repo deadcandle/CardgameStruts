eb3361c805ed75a72f83a4a87e99aac9fb7b4836
Misha <mho-sam-sooi@student.scalda.nl>
2022-06-16 08:45:02 +0200

    test

--- src/main/java/prg/Deck.java
+++ src/main/java/prg/Deck.java
@@ -10,6 +10,7 @@ public class Deck {
         for (int i = 0; i < 4; i++) {
             for (int j = 2; j <= 10; j++) {
                 switch (i) {
+                    // bruh
                     case 0: {Card c = new Card("Clubs",j +" of Clubs",j); cards.add(c); break;}
                     case 1: {Card c = new Card("Diamonds",j+" of Diamonds",j); cards.add(c); break;}
                     case 2: {Card c = new Card("Hearts",j+" of Hearts",j); cards.add(c); break;}
