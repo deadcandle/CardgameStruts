62ba459d52affd85a8860188bbb0e740edae9ff1
Boris Sanderse <sanderseboris01@gmail.com>
2022-06-20 10:38:08 +0200

    Update MySQLConnector.java
    dom aan het doen

--- src/main/java/model/MySQLConnector.java
+++ src/main/java/model/MySQLConnector.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLConnector {
+    public void connect() {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cardgame", "root", "boris");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
